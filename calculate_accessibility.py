537e2a21a6d61245cfb49f43e59fc6b53fc2e93f
Christine Guo <67035166+christine-guo@users.noreply.github.com>
2023-01-19 22:10:24 -0500

    Add files via upload

---
+++ calculate_accessibility.py
@@ -0,0 +1,14 @@
+
+for annotation in annotations:
+
+
+'''
+formulas to calculate the accessibility:
+
+with stairs:
+level = 0.6(min(stair_list)) + 0.4(min(door_list))
+
+with ramp:
+level = 0.6(min(ramp_list)) + 0.4(min(door_list))
+
+'''
